0591cdf8af2312013f5353bbc4a50fd9870e159d
siddharth0600 <siddharthtripathi00@gmail.com>
2022-06-03 02:18:59 +0530

    Button creation and theme creation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import { Button } from "@mui/material";
-
+import { Button, Typography, styled } from '@mui/material'
+import { Add, Settings } from '@mui/icons-material'
 function App() {
+  const BlueButton = styled(Button)(({ theme }) => ({
+    backgroundColor: 'skyblue',
+    color: '#888',
+    margin: 5,
+    '&:hover': {
+      backgroundColor: 'lightblue',
+    },
+    '&:diabled': {
+      backgroundColor: 'gray',
+      color: 'white',
+    },
+  }))
   return (
     <div>
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button
+        startIcon={<Settings />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Settings
+      </Button>
+      <Button
+        startIcon={<Add />}
+        variant="contained"
+        color="success"
+        size="small"
+      >
+        Add new Post
+      </Button>
+      <Button variant="outlined" diabled>
+        Outlined
+      </Button>
+      <Typography variant="h1" component="p">
+        h1. Heading
+      </Typography>
+
+      <BlueButton>This is a blue button</BlueButton>
+      <BlueButton>This is a blue button</BlueButton>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { ThemeProvider } from '@mui/material'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import App from "./App";
+import App from './App'
+import { theme } from './theme'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
